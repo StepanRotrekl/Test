5b795c61e4d394504cffb67ea564c503358d4bac
rotste17 <rotste17@gasos-ro.cz>
2025-02-25 12:25:28 +0100

    blah

--- src/autosaloon/Car.java
+++ src/autosaloon/Car.java
@@ -1,6 +1,40 @@
 package autosaloon;
 
 public class Car {
+    private int year;
+    private int mileage;
+    private String color;
+    private Condition condition;
 
-	
+    public Car(int year, int mileage, String color, Condition condition) {
+        this.year = year;
+        this.mileage = mileage;
+        this.color = color;
+        this.condition = condition;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public int getMileage() {
+        return mileage;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public Condition getCondition() {
+        return condition;
+    }
+
+
+    public String toString() {
+        return "Roky: " + year + "Delka: " + mileage + "Barva: " + color + "Kondice: " + condition;
+
+
+
+    }
 }
+
--- src/autosaloon/CarDatabase.java
+++ src/autosaloon/CarDatabase.java
@@ -1,24 +1,48 @@
 package autosaloon;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
 public class CarDatabase {
 
     String owner;
     Car[] cars;
 
+    public CarDatabase(String owner) {
+        this.owner = owner;
+
+    }
 
     /**
      * Metoda se zepta uzivatele na informace k pozadovanemu poctu aut. A ty prida do pole aut.
      * @param count - pozadovany pocet aut
      */
     public void addCars(int count) {
-        //  cars = new ....
-        for (int i=0; i<count; count++){
-            //nacti od uzivatele rok vyroby, pocet najetych km, barvu a stav auta
-            //vytvor instanci auta - nutno upravit i tridu Car, chceme instanci nastavit atributy na nactene hodnoty od uzivatele
-            //pridej auto do pole aut
-        }
+        Scanner scanner = new Scanner(System.in);
 
+        for (int i = 0; i < count; i++) {
+            System.out.println("Zadejte údaje pro auto č. " + (i + 1));
+
+            System.out.print("Rok výroby: ");
+            int year = scanner.nextInt();
+
+            System.out.print("Najeté kilometry: ");
+            int mileage = scanner.nextInt();
+
+            scanner.nextLine();
+
+            System.out.print("Barva: ");
+            String color = scanner.nextLine();
+
+            System.out.print("Stav (NEW, USED, DAMAGED, EXCELLENT): ");
+            Condition condition = Condition.valueOf(scanner.nextLine().toLowerCase());
+
+            cars [i] = new Car(year, mileage, color, condition);
+        }
     }
+
 
     /**
      * Vypise vsechna auta, jejijz stav je "good". Vypis bude ve formatu: Na prvnim radku bude vypis: Dobra auta jsou: .
@@ -27,4 +51,5 @@ public class CarDatabase {
     public void printCarInGoodCondition(){
 
     }
+
 }
